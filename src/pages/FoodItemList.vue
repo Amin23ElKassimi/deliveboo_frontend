cb188c40ee4ad789e3538fdc8a213a7cb942c5a5
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-03-12 14:50:04 +0100

    routes

---
+++ src/pages/FoodItemList.vue
@@ -0,0 +1,97 @@
+<template lang="">
+    <main class="container-fluid">
+        <section class="container">
+                    <select v-model="categoriesToFilter" @change="archetypeToFilter" name="selectCategory" id="selected-card" class="form-select" aria-label="Default select example">
+                        <option value="all">Tutti</option>
+                        <option v-for="category in categories" :value="category.name">{{ category.name }}</option>
+                </select>
+                </section>
+        <!-- Lista Film da stampare come Cards -->
+        <div class="lista row justify-content-center"> 
+            <SingleCard class="card p-0 col-3 mx-4 my-5" v-for="restaurant in restaurants" v-show="elementToShow(restaurant.categories)" :key="restaurant.id"
+        :name="restaurant.name" :vat="restaurant.vat" :address="restaurant.address"  :email="restaurant.email" :image_url="restaurant.image_url" :phone_number="restaurant.phone_number"/>            
+        </div>
+    </main>
+</template>
+
+
+<script>
+// Import Azion
+import SingleCard from '@/components/SingleCard.vue';
+
+import axios from 'axios';
+
+export default {
+    name: 'RestaurantList',
+
+    data(){
+        return{
+            restaurants: [],
+            categories: [],
+            categoriesToFilter: 'all',
+        }
+    },
+
+    methods:{
+        getFoodItems(){
+            axios.get('http://127.0.0.1:8000/api/FoodItems', {
+                params: {
+                }
+            })
+            .then((response) => {
+                console.log('ristoranti', response.data.results.data);
+                this.restaurants = response.data.results.data;
+
+            })
+            .catch(function (error) {
+                console.warn(error);
+            })
+        },
+
+    },
+
+    components:{
+        SingleCard
+    },
+
+    created(){
+        this.getRestaurants();
+        this.getCategories();
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+
+img{
+    width: 100%;
+}
+
+main {
+        background-color: $primary-bg;
+        padding: 50px;
+    }
+
+    .lista{
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .cards{
+        color: white;
+        width: calc(100% / 5 - 1rem);
+        padding: .5rem;
+        margin-left: .5rem;
+        margin-right: .5rem;
+        margin-bottom: 1rem;
+        background-color: $primary-bg;
+        background-color: #521c76;
+        text-align: center;
+    }
+
+    .title{
+        color: yellow;
+    }
+    </style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import AppHome from './pages/HomePage.vue';
 import RestaurantList from './pages/RestaurantList.vue';
 import SingleRestaurant from './pages/SingleRestaurant.vue';
 import CategoriesList from './pages/CategoriesList.vue';
+import FoodItemList from './pages/FoodItemList.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -27,6 +29,11 @@ const router = createRouter({
             path: '/categories',
             name: 'categories',
             component: CategoriesList
+        },
+        {
+            path: '/fooditems',
+            name: 'fooditems',
+            component: FoodItemList
         },
     ]
 });

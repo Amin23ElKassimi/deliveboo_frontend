57be897030eb5860823ffa3fc9cb2458e598c286
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-03-14 11:56:21 +0100

    little changes

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,27 +1,30 @@
 <template lang="">
-<div class="card" style="width: 18rem;">
-    <img class="card-img-top" :src="image_url" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">{{ name }}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">{{ address }}</h6>
-      <p class="card-text">{{ phone_number }}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div id="cards" class="card style" >
+        <img class="card-img-top" :src="image_url" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Nome Risto{{ name }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{ category }}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">{{ address }}</h6>
+        <p class="card-text">{{ phone_number }}</p>
+        <div class="w-100 d-flex mb-3" v-if="linkRoute">
+            <router-link :to="linkRoute" class="btn btn-primary">
+                {{ linkLabel }}
+            </router-link>
+        </div>
+        </div>
+        
     </div>
-</div>
-<!-- <img v-if="image" :src="image" class="card-img-top mb-3" :class="fullLength ? '' : 'image-preview'" alt="...">
-<p class="card-text" v-if="fullLength">
-    {{ vat }}
-</p>
-<p v-else>
-    {{ content.substring(0, 120) }}...
-</p> -->
-<!-- <div class="w-100 d-flex mb-3" v-if="linkRoute">
-    <router-link :to="linkRoute" class="btn btn-primary">
-        {{ linkLabel }}
-    </router-link>
-</div> -->
-
+    <!-- Lista Film da stampare come Cards -->
+    <div v-for="fooditem in menu">
+        <img :src="fooditem.image_url" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"  >{{ fooditem.name }}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>    
+        </div>
+    </div>
 </template>
+
 <script>
 export default {
     data() {
@@ -58,21 +61,26 @@ export default {
             required: true,
             type: String,
         },
-        // linkRoute: {
-        //     required: false,
-        //     type: [ Object, String ]
-        // },
-        // linkLabel: {
-        //     required: false,
-        //     type: String,
-        // },
-        // fullLength: {
-        //     required: false,
-        //     type: Boolean,
-        // },
+        menu: {
+            required: true,
+            type: String,
+        },
+        linkRoute: {
+            required: false,
+            type: [ Object, String ]
+        },
+        linkLabel: {
+            required: false,
+            type: String,
+        },
+        fullLength: {
+            required: false,
+            type: Boolean,
+        },
     }
 }
 </script>
+
 <style lang="scss" scoped>
     img{
         height: 400px;
@@ -83,4 +91,11 @@ export default {
     img.image-preview{
         height: 200px;
     }
+    #cards{
+        width: 18rem;
+        margin: 1rem;
+    }
+    
+
+
 </style>
--- src/pages/SingleRestaurant.vue
+++ src/pages/SingleRestaurant.vue
@@ -1,87 +1,85 @@
 <template lang="">
     <main class="container">
-        <section class="container">
-            <!-- Lista Film da stampare come Cards -->
-            <div class="lista row justify-content-center"> 
-                <SingleCard class="card p-0 col-3 mx-4 my-5" v-for="restaurant in restaurants" :key="restaurant.id" :name="restaurant.name" :vat="restaurant.vat" :address="restaurant.address"  :email="restaurant.email" :image_url="restaurant.image_url" :phone_number="restaurant.phone_number"/>            
-            </div>
+        <section class="row justify-content-center">
+            <SingleCard class="p-0 col-12 mx-4 my-5" 
+            :name="restaurant.name" :vat="restaurant.vat"
+            :menu="restaurant.food_item"
+            :address="restaurant.address"  :email="restaurant.email"
+            :image_url="restaurant.image_url" :phone_number="restaurant.phone_number"/>
         </section>
     </main>
 </template>
 
-
 <script>
-// Import Azion
 import SingleCard from '@/components/SingleCard.vue';
 
 import axios from 'axios';
 
 export default {
-    name: 'RestaurantList',
-
+    name: 'PostList',
     data(){
         return{
-            restaurant: [],
+            restaurants: [],
+            restaurant: {},
             id: ''
         }
     },
-
     methods:{
-        // getRestaurants(){
-        //     axios.get(`http://127.0.0.1:8000/api/restaurants/${id}`, {
-        //         params: {
-        //         }
-        //     })
-        //     .then((response) => {
-        //         console.log('ristoranti', response.data.results1.data);
-        //         this.restaurants = response.data.results1.data;
+        getResturant(){
+            axios.get(`http://127.0.0.1:8000/api/restaurants/${this.$route.params.id}`, {
+                params: {
+                }
+            })
+            .then((response) => {
+                console.log(this.$route.params.id);
+                // console.log(response.data.results);
+                this.restaurant = response.data.results;
 
-        //     })
-        //     .catch(function (error) {
-        //         console.warn(error);
-        //     })
-        // },
+            })
+            .catch(function (error) {
+                console.warn(error);
+             })
+        },
+        getFoodItems(){
+            axios.get('http://127.0.0.1:8000/api/foodItems', {
+                params: {
+                }
+            })
+            .then((response) => {
+                // console.log(response.data.results);
+                this.fooditems = response.data.results;
+            })
+            .catch(function (error) {
+                console.warn(error);
+            })
+        },
     },
     components:{
-        SingleCard
+        SingleCard,
     },
+
+    props:{
+        name: {
+            required: true,
+            type: String,
+        },
+        image_url: {
+            required: true,
+            type: String,
+        },
+        menu: {
+            required: true,
+            type: String,
+        },
+    },
+
     created(){
-        // this.getRestaurants();
+        console.log(this.menu);
+        this.getResturant();
+        this.getFoodItems();
     }
 }
 </script>
 
-
-<style lang="scss" scoped>
-@use '../styles/partials/variables.scss' as *;
-
-img{
-    width: 100%;
-}
-
-main {
-        background-color: $primary-bg;
-        padding: 50px;
-    }
-
-    .lista{
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-    .cards{
-        color: white;
-        width: calc(100% / 5 - 1rem);
-        padding: .5rem;
-        margin-left: .5rem;
-        margin-right: .5rem;
-        margin-bottom: 1rem;
-        background-color: $primary-bg;
-        background-color: #521c76;
-        text-align: center;
-    }
-
-    .title{
-        color: yellow;
-    }
-    </style>
+<style lang="scss">
+</style>

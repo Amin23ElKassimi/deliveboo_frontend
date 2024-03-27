d62e682e90614209eea2a3ff888ecacc299657f9
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-03-27 12:30:34 +0100

    modified appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,19 +3,20 @@
         <nav class="navbar navbar-graphic navbar-expand-lg " data-bs-theme="dark">
             <div class="container d-flex justify-content-between">
                 <div class="d-flex align-items-center">
-                    <img class="logo-img rounded-circle" src="/Logo_Deliveboo.jpg" alt="immagine logo">
-                    <a class="ms-2 navbar-brand">DeliveBoo</a>
-                </div>
-                <div class= "w-60" id="navbarNavDropdown">
+                    <router-link to="/">
+                        <img class="logo-img rounded-circle" src="/Logo_Deliveboo.jpg" alt="immagine logo">
+                        <a class="ms-2 navbar-brand" >DeliveBoo</a>
+                    </router-link>
+                    
                     <ul class="navbar-nav">
                         <li class="nav-item me-2" v-for="(link, index) in linkItems" :key="index">
                             <router-link :to="{ name: link.routeName }" class="nav-link">
                                 {{ link.label }}
                             </router-link>
                         </li>
-                        <a class="nav-link text-decoration-none" href="http://127.0.0.1:8000/login" >Area Ristoratore</a>
                     </ul>
                 </div>
+                <a class="nav-link text-decoration-none" href="http://127.0.0.1:8000/login" >Area Ristoratore</a>
             </div>
         </nav>
 
@@ -32,10 +33,6 @@ export default {
     data() {
         return {
             linkItems: [
-                {
-                    label: 'Home',
-                    routeName: 'home'
-                },
                 {
                     label: 'Ristoranti',
                     routeName: 'restaurants'
@@ -63,6 +60,10 @@ export default {
         width: 30px;
         height: 30px;
 
+    }
+
+    a.navbar-brand{
+        text-decoration: none;
     }
 
     @media only screen and (max-width: 991.2px) {

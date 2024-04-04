8e6db28f3c8d99fa6f9a757ed090ad71f46bf015
UmbertoSerino <serumberto@gmail.com>
2024-04-04 15:51:59 +0200

    Send Order Success (no paymant)

--- src/pages/OrderCheckout.vue
+++ src/pages/OrderCheckout.vue
@@ -1,34 +1,29 @@
 <template>
     <main>
         <div class="container">
-            <!-- <div class="col-12 mt-2"> -->
-            <form class="row">
+            <form class="row" @submit.prevent="pagamento">
                 <div class="col-6 mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Nome e Cognome</label>
-                    <input type="text" class="form-control" id="customer" name="customer">
+                    <label for="customer" class="form-label">Nome e Cognome</label>
+                    <input type="text" class="form-control" id="customer" v-model="customer" name="customer">
                 </div>
                 <div class="col-6 mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Indirizzo</label>
-                    <input type="text" class="form-control" id="user_address" name="user_address">
+                    <label for="user_address" class="form-label">Indirizzo</label>
+                    <input type="text" class="form-control" id="user_address" v-model="user_address" name="user_address">
                 </div>
                 <div class="col-6 mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Telefono</label>
-                    <input type="text" class="form-control" id="user_phone_number" name="user_phone_number">
+                    <label for="user_phone_number" class="form-label">Telefono</label>
+                    <input type="text" class="form-control" id="user_phone_number" v-model="user_phone_number" name="user_phone_number">
                 </div>
                 <div class="col-6 mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="user_mail" name="user_mail">
+                    <label for="user_mail" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="user_mail" v-model="user_mail" name="user_mail">
                 </div>
                 <!-- DROP IN -->
                 <div class="col-7">
-                    <div id="dropin-container">
-
-                    </div>
+                    <div id="dropin-container"></div>
                 </div>
                 <div class="col-5">
-                    <h1>
-                        Resoconto
-                    </h1>
+                    <h1>Resoconto</h1>
                     <ul class="list-group" v-for="food_item in carrello" :key="food_item.id">
                         <li class="list-group-item d-flex justify-content-between align-items-center">
                             {{ food_item.name }}
@@ -40,8 +35,7 @@
                     <h4>Totale: {{ calculatePrice() }} €</h4>
                 </div>
                 <div>
-                    <button type="submit" id="submit-button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" @click.prevent="pagamento">Paga
-                        ora</button>
+                    <button type="submit" id="submit-button" class="btn btn-primary">Paga ora</button>
                 </div>
             </form>
             <!-- Modale -->
@@ -53,7 +47,7 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <p>Il tuo ordine è già in viaggio!</p>
+                            <p>Complimenti, il tuo ordine è già in viaggio!</p>
                             <!-- Immagine dello scooter animata -->
                             <img src="../../public/IMG/giphy.gif" alt="Scooter" id="scooter-image">
                         </div>
@@ -64,45 +58,66 @@
                 </div>
             </div>
         </div>
-        <!-- </div> -->
     </main>
 </template>
 
 <script>
-import { store } from '@/store';
 import BraintreeDropin from 'braintree-web-drop-in';
 import axios from 'axios';
-import RestaurantList from './RestaurantList.vue';
 
 const braintreeToken = 'rwrht7y7y5crzp9h';
 
 export default {
     name: 'OrderCheckout',
-    // props: {
-    //     carrello: {
-    //         type: Array,
-    //         required: true,
-    //     },
-    // },
     data() {
         return {
-            store,
-            totaleCarrello: '',
-            braintreeInstance: null,
-            carrello: JSON.parse(localStorage.getItem('carrello')) || [], // Carica il carrello dall'localStorage
-
+            customer: '',
+            user_address: '',
+            user_phone_number: '',
+            user_mail: '',
+            // restaurant_id: '',
+            status: true,
+            carrello: JSON.parse(localStorage.getItem('carrello')) || []
         }
     },
     methods: {
+        calculatePrice() {
+            let total = 0;
+            this.carrello.forEach(articolo => {
+                total += parseFloat(articolo.price);
+            });
+            return total.toFixed(2);
+        },
+        pagamento() {
+            const orderData = {
+                user_address: this.user_address,
+                customer: this.customer,
+                user_phone_number: this.user_phone_number,
+                user_mail: this.user_mail,
+                status: this.status,
+                total: this.calculatePrice(),
+                restaurant_id: this.carrello.length > 0 ? this.carrello[0].restaurant_id : null,
+                food_items: this.carrello.map(item => ({
+                    food_id: item.id,
+                    // quantity: item.quantity,
+                })),
+            };
+            console.log(orderData, 'questo è quello che contiene il carrello', this.carrello);
+
+            axios.post('http://127.0.0.1:8000/api/orders', orderData)
+                .then(response => {
+                    console.log('Ordine inviato con successo!', response.data, orderData);
+                    this.svuotaCarrello();
+                    $('#exampleModal').modal('show');
+                })
+                .catch(error => {
+                    console.error('Errore durante l\'invio dell\'ordine:', error.response.data);
+                    // Gestisci eventuali errori qui
+                });
+        },
         svuotaCarrello() {
             localStorage.removeItem('carrello');
         },
-        pagamento() {
-            console.log('PAGAAAHHHHHH')
-            this.svuotaCarrello();
-            this.$router.push('/restaurants');
-        },
-        // Brain tree create
         initBraintree() {
             BraintreeDropin.create({
                 authorization: braintreeToken,
@@ -114,18 +129,8 @@ export default {
                 }
                 this.braintreeInstance = instance;
             });
-        },
-        calculatePrice() {
-            let total = 0;
-            this.carrello.forEach(articolo => {
-                total += parseFloat(articolo.price);
-            });
-            return total;
         }
-
-        // this.store.currentRestaurant = null;
     },
-
     mounted() {
         this.initBraintree();
         let button = document.querySelector('#submit-button');
@@ -158,6 +163,7 @@ export default {
     }
 }
 </script>
+
 <style lang="scss" scoped>
 #dropin-container {
     width: 100%;
